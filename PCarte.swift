028fb89b73e185162ebdc6d83e073c4c27e179c7
MathildeTrb <57673677+MathildeTrb@users.noreply.github.com>
2019-11-28 21:17:14 +0100

    Update PCarte.swift
    ajout description  affiche carte

--- PCarte.swift
+++ PCarte.swift
@@ -25,7 +25,14 @@ protocol PCarte {
 	func contient (pos : PPosition) -> Bool
 
 	// afficheCarte : TCarte x Int -> String 
-	// Post : retourne une chaîne de caractères contenant le nom de la carte et une grille explicitant les déplacements possibles comme sur le sujet
+	// Post : retourne une chaîne de caractères contenant :
+	// le nom de la carte 
+	// une position de référence (ou est placé une pièce fictive qui servira de référence
+	// les positions possibles de cette pièce référence
+	// le sens est un entier qui a pour valeur soit 1 soit -1
+	// si le sens vaut 1 cela concerne le joueur Bleu qui a ses pions en bas du plateau lorsque ce dernier est " affiché "
+	// si le sens vaut -1 cela concerne le joueur Rouge qui a ses pions en haut du plateau lorsque ce dernier est "affiché"
+	// pour se dernier cas il faut donc pensez que les positions possibles sont inversées
 	func afficheCarte (sens : Int) -> String
 }
 
