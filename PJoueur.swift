ff7f177223703edc7d711bae72ee7c4a5e733332
MathildeTrb <57673677+MathildeTrb@users.noreply.github.com>
2019-11-28 21:28:12 +0100

    Update PJoueur.swift

--- PJoueur.swift
+++ PJoueur.swift
@@ -1,55 +1,56 @@
 protocol PJoueur {
 
 
-	// init : String x String x TOnimata -> PJoueur
-	// Créer un joueur dont le nom est newNom, dont la couleur est newCouleur et qui a en sa possession un maitre et 4 pion de sa couleur (qui peuvent être récupérés directement sur le plateau entré en paramètre)
+	// init : String x String x POnimata -> PJoueur
+	// Créer un joueur dont le nom est newNom, dont la couleur est newCouleur et qui a en sa possession un maitre et 4 pion de sa couleur 
+	// (qui peuvent être récupérés directement sur le plateau entré en paramètre, qui est le plateau de jeu courant)
 	// Pre: La couleur du joueur est soit "Rouge" soit "Bleu"
 	// Pre: le nom du joueur ne peut être vide
 	// Resultat : un joueur avec un nom, une couleur et des pièces associées
 	init (newNom : String, newCouleur : String, plateau : POnimata)
 
-	// aPourNom : TJoueur -> String
+	// aPourNom : PJoueur -> String
 	// donne le nom du joueur 
 	// Resultat : un string correspondant au nom du joueur
 	var aPourNomJ : String {get}
 
-	// aPourCouleurJ : TJoueur -> String 
+	// aPourCouleurJ : PJoueur -> String 
 	// donne la couleur du joueur (la couleur sera "Rouge" ou "Bleu")
 	// Resultat : un string correspondant à la couleur du joueur 
 	var aPourCouleurJ : String {get}
 	
-	// aPourPieces : TJoueur -> [PPiece]
+	// aPourPieces : PJoueur -> [PPiece]
 	// Donne sous forme de tableau tous les pions encore présent sur le plateau d'un joueur
-	// Pre: La couleur de toutes les pièces et la même que celle du joueur
+	// Pre: La couleur de toutes les pièces est la même que celle du joueur
 	var aPourPieces : [PPiece] {get set}
 
-	// aPourCarte1 : TJoueur -> TCarte 
+	// aPourCarte1 : PJoueur -> PCarte 
 	// donne la carte 1 du joueur 
-	// Resultat : une TCarte qui correspond à la carte1 du joueur 
+	// Resultat : une PCarte qui correspond à la carte1 du joueur 
 	var aPourCarte1 : PCarte! {get set}
 
-	// aPourCarte2 : TJoueur -> TCarte 
+	// aPourCarte2 : PJoueur -> PCarte 
 	// donne la carte 2 du joueur 
-	// Resultat : une TCarte qui correspond à la carte2 du joueur
+	// Resultat : une PCarte qui correspond à la carte2 du joueur
 	var aPourCarte2 : PCarte! {get set}
 
-	// recupPiece : String -> TPiece
+	// recupPiece : String -> PPiece
 	// Resultat : cette fonction renvoie la pièce correspondant au String ssi elle appartient au joueurCourant (ce qui implique qu'elle n'a pas été éliminée)
-	// Pre : le String reçu en paramètre doit correspondre à une pièce que le joueur détient
+	// Pre : le String entré en paramètre doit correspondre au nom d'une pièce que le joueur détient
 	// Post : une Pièce correspondant à la chaîne de caractère est retournée
 	func recupPiece (pieceSaisie : String) -> PPiece
 
-	// elimine : Tpiece x TJoueur -> TJoueur 
+	// elimine : Ppiece x PJoueur -> TJoueur 
 	// Est utilisée lorsqu'une pièce est éliminé de la partie
 	// Resultat : enlève au joueur la pièce entrée en paramètre
 	mutating func elimine (piece : PPiece)
 
-	// autreJoueur : TJoueur -> TJoueur
+	// autreJoueur : PJoueur -> PJoueur
 	// Resultat : Renvoie le joueur qui n'est pas le joueur courant
 	mutating func autreJoueur()
 
-	// peutJouer : TJoueur -> Bool
-	// Resultat : Si au moins un déplacement est possible pour l'une des deux cartes ainsi que l'une des pièces du joueur, renvoie True, sinon False
+	// peutJouer : PJoueur -> Bool
+	// Resultat : Si au moins une nouvelle position est possible pour l'une des deux cartes ainsi que l'une des pièces du joueur, renvoie True, sinon False
 	func peutJouer () -> Bool
 
 }
